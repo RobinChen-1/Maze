b447bb268f1bf0e262c87d03c2c4ece91aac7924
RobinChen-1 <27ChenR@mbbc.qld.edu.au>
2025-08-29 10:57:30 +1000

    1

---
+++ Rooms/WelcomeScreen.py
@@ -0,0 +1,10 @@
+from GameFrame import Level
+from Objects.Title import Title
+
+class WelcomeScreen(Level):
+    def __init__(self, screen, joysticks):
+        Level.__init__(self, screen, joysticks)
+
+        self.set_background_image("Background.png")
+
+        self.add_room_object(Title(self,420,10))
